00ea648261d79c9f39a0f410c04362edd6ba532e
Emmanuel Guefif <eguefif@fastmail.com>
2025-12-01 09:14:59 +0100

    day1: problem 2

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,10 +1,11 @@
 fn main() {
     let input = std::fs::read_to_string("./input.txt").unwrap();
     let parsed = parse(input);
-    println!("Res p1: {}", solve_p1(parsed));
+    println!("Res p1: {}", solve_p1(parsed.clone()));
+    println!("Res p2: {}", solve_p2(parsed));
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Direction {
     Left(i32),
     Right(i32)
@@ -27,7 +28,6 @@ fn parse(input: String) -> Vec<Direction> {
 fn solve_p1(directions: Vec<Direction>) -> i32 {
     let mut result = 0;
     let mut code = 50;
-    let mut s = String::new();
     for direction in directions {
         match direction {
             Direction::Left(v) => code = wrapping_sub(code, v),
@@ -52,6 +52,43 @@ fn wrapping_sub(a :i32, b: i32) -> i32 {
     res
 }
 
+fn solve_p2(directions: Vec<Direction>) -> u32 {
+    let mut result = 0;
+    let mut code = 50;
+    for direction in directions {
+        let zero: u32;
+        match direction {
+            Direction::Left(v) => (code, zero) = wrapping_sub_zero(code, v),
+            Direction::Right(v) => (code, zero) = wrapping_sum_zero(code, v),
+        }
+        result += zero;
+    }
+    result
+}
+
+fn wrapping_sum_zero(a: i32, b: i32) -> (i32, u32) {
+    let flag = if a == 100 { 1 } else { 0 };
+    let mut res = a + b;
+    let mut zero = 0;
+    while res > 99 {
+        zero += 1;
+        res -= 100;
+    }
+    (res, zero - flag)
+
+}
+
+fn wrapping_sub_zero(a :i32, b: i32) -> (i32, u32) {
+    let flag = if a == 0 { 1 } else { 0 };
+    let mut res = a - b;
+    let mut zero = 0;
+    while res <= 0 {
+        zero += 1;
+        res += 100;
+    }
+    (res, zero - flag)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -62,7 +99,16 @@ mod tests {
         let directions = parse(input);
         let res = solve_p1(directions);
 
-        assert_eq!(true, false);
         assert_eq!(res, 3);
+    }
+
+    #[test]
+    fn test_2(){
+        let input = std::fs::read_to_string("./src/day1/test.txt").unwrap();
+        let directions = parse(input);
+        let res = solve_p2(directions);
+
+        //assert_eq!(true, false);
+        assert_eq!(res, 6);
     }
 }
