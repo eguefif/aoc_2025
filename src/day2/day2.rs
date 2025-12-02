40e8331ea31360004ba590dd6d8149aaeb5e2a79
Emmanuel Guefif <eguefif@fastmail.com>
2025-12-02 08:34:23 +0100

    day2: problem 2 with v2 solver This solver calculate directly the result without passing by a string. It's twice as fast than the first solver.

--- src/day2/day2.rs
+++ src/day2/day2.rs
@@ -1,10 +1,19 @@
+use std::time::Instant;
 type RangeList = Vec<(u64, u64)>;
 
 fn main() {
     let input = std::fs::read_to_string("./src/day2/input.txt").unwrap();
     let parsed = parse(input);
+
     println!("problem 1: {}", solve(parsed.clone(), is_invalid_id_1));
-    println!("problem 2: {}", solve(parsed.clone(), is_invalid_id_2));
+    let start = Instant::now();
+    let result = solve(parsed.clone(), is_invalid_id_2);
+    let elapsed = start.elapsed().as_millis();
+    println!("problem 2: {} solved in {}ms", result, elapsed);
+    let start = Instant::now();
+    let result = solve(parsed.clone(), is_invalid_id_2_v2);
+    let elapsed = start.elapsed().as_millis();
+    println!("problem 2 with v2: {} solved in {}ms", result, elapsed);
 }
 
 fn parse(input: String) -> RangeList {
@@ -73,7 +82,36 @@ fn is_invalid_id_2(id: u64) -> bool {
             return false;
         }
     }
-    false
+}
+
+fn is_invalid_id_2_v2(id: u64) -> bool {
+    let mut pattern_size = 1;
+    let size = get_size(id);
+    'outer: loop {
+        if (size / 2) < pattern_size {
+            return false
+        }
+
+        let mut div = 10u64.pow(pattern_size);
+        let pattern = id % 10u64.pow(pattern_size);
+        if get_size(pattern) != pattern_size {
+            pattern_size += 1;
+            continue;
+        }
+        let mut num = id / div;
+        loop {
+            let chunk = num % div;
+            if chunk != pattern {
+                pattern_size += 1;
+                continue 'outer
+            }
+            num /= div;
+            if num == 0 {
+                break;
+            }
+        }
+        return true;
+    }
 }
 
 #[cfg(test)]
@@ -92,6 +130,13 @@ mod tests {
         let input = std::fs::read_to_string("./src/day2/input.txt").unwrap();
         let parsed = parse(input);
         assert_eq!(29940924880, solve(parsed, is_invalid_id_1));
+    }
+
+    #[test]
+    fn it_should_solve_2_real_input_v2() {
+        let input = std::fs::read_to_string("./src/day2/input.txt").unwrap();
+        let parsed = parse(input);
+        assert_eq!(48631958998, solve(parsed, is_invalid_id_2_v2));
     }
 
     #[test]
@@ -132,6 +177,12 @@ mod tests {
     }
 
     #[test]
+    fn it_should_get_invalid_2_v2_22323() {
+        let result = is_invalid_id_2(22323);
+        assert_eq!(result, false);
+    }
+
+    #[test]
     fn it_should_get_invalid_2_false_1111() {
         let result = is_invalid_id_2(1111);
         assert_eq!(result, true);
@@ -165,5 +216,41 @@ mod tests {
     fn it_should_get_invalid_2_true_22() {
         let result = is_invalid_id_2(22);
         assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_123123123() {
+        let result = is_invalid_id_2_v2(123123123);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_1111() {
+        let result = is_invalid_id_2_v2(1111);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_1212() {
+        let result = is_invalid_id_2_v2(1212);
+        assert_eq!(result, true);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_1211() {
+        let result = is_invalid_id_2_v2(1211);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_1() {
+        let result = is_invalid_id_2_v2(1);
+        assert_eq!(result, false);
+    }
+
+    #[test]
+    fn it_should_get_invalid_2_v2_false_80808() {
+        let result = is_invalid_id_2_v2(80808);
+        assert_eq!(result, false);
     }
 }
