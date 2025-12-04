a22240687bb33747b7c27215185ea76c1b770b9f
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-12-04 09:59:41 +0100

    day4: part2

--- src/day4/day4.rs
+++ src/day4/day4.rs
@@ -5,6 +5,8 @@ fn main() {
     let parsed = parse(input);
     let res = solve1(parsed.clone());
     println!("Res!: {}", res);
+    let res = solve2(parsed);
+    println!("Res!: {}", res);
 }
 
 fn parse(input: String) -> HashSet<(i32, i32)> {
@@ -22,7 +24,7 @@ fn parse(input: String) -> HashSet<(i32, i32)> {
 fn solve1(input: HashSet<(i32, i32)>) -> u32 {
     let mut res = 0;
     for roll in input.iter() {
-        if check_1(roll, &input) {
+        if check(roll, &input) {
             res += 1;
         }
 
@@ -30,7 +32,34 @@ fn solve1(input: HashSet<(i32, i32)>) -> u32 {
     res
 }
 
-fn check_1((x, y): &(i32, i32), input: &HashSet::<(i32, i32)>) -> bool {
+fn solve2(input: HashSet<(i32, i32)>) -> u32 {
+    let mut res = 0;
+    let mut size = input.len();
+    let mut new_set = input.clone();
+    loop {
+        new_set = trim_down(new_set);
+        if size - new_set.len() == 0 {
+            break;
+        }
+        res += size - new_set.len();
+        size = new_set.len();
+    }
+    res as u32
+}
+
+fn trim_down(set: HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
+    let mut new_set: HashSet<(i32, i32)> = HashSet::new();
+    
+    for entry in set.iter() {
+        if !check(entry, &set) {
+            new_set.insert(*entry);
+        }
+    }
+
+    new_set
+}
+
+fn check((x, y): &(i32, i32), input: &HashSet::<(i32, i32)>) -> bool {
     let mut res = 0;
     if input.contains(&(*x, *y + 1)) { res += 1 }
     if input.contains(&(*x + 1, *y + 1)) { res += 1 }
@@ -54,5 +83,13 @@ mod tests {
         let parsed = parse(input);
         let res = solve1(parsed);
         assert_eq!(res, 13);
+    }
+
+    #[test]
+    fn test_example2() {
+        let input = std::fs::read_to_string("./src/day4/test.txt").unwrap();
+        let parsed = parse(input);
+        let res = solve2(parsed);
+        assert_eq!(res, 43);
     }
 }
